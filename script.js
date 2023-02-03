04596b02e0e1118cce745a73969342f689c52dfc
Lasya Gadiyaram <lasyagadiyaram@gmail.com>
2023-02-03 22:26:30 +0530

    Resto-Website

---
+++ script.js
@@ -0,0 +1,10 @@
+let food1s = document.getElementById('food1');
+let food2s = document.getElementById('food2');
+let foods = document.getElementById('food');
+
+food1s.addEventListener('click', () => {
+    foods.style.backgroundImage = "url('./pizza1.jpg')";
+})
+food2s.addEventListener('click', () => {
+    foods.style.backgroundImage = "url('./spring-roll.jpg')";
+})
